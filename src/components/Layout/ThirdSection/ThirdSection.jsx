7013a6f253b6301915cd049021675d875e67e050
Josivaldo Soares <77576339+JosivaldoS@users.noreply.github.com>
2025-02-11 11:28:02 -0300

    Adicionando texto sobre mim
    Adicionei Conteúdo na aba Sobre mim, sendo eles um texto breve sobre minha experiência
    Adicionei um Placeholder para Adicionar uma imagem no futuro

--- src/components/Layout/ThirdSection/ThirdSection.jsx
+++ src/components/Layout/ThirdSection/ThirdSection.jsx
@@ -4,6 +4,18 @@ export default function ThirdSection(){
     return(
         <main class='bg-CorPreta text-white w-screen h-[800px] overflow-hidden'>
             <h2 class='text-center text-6xl mt-9'>Sobre mim</h2>
+            <div>
+                <div>
+                    <p>Sou Desenvolvedor Front End, especializado em React, e atualmente curso Análise e Desenvolvimento de Sistemas. Embora ainda não tenha experiência profissional na área, estou constantemente aprimorando minhas habilidades por meio de projetos práticos e estudos contínuos. Sou proativo, dedicado e estou ansioso para aplicar meu conhecimento em um ambiente profissional, contribuindo com minha paixão por tecnologia e aprendizado constante. 🚀
+                    </p>
+                </div>
+                <div>
+                    <img 
+                        src="https://placehold.co/400" 
+                        alt="" 
+                    />
+                </div>
+            </div>
         </main>
     )
 

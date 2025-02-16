4c4b417f36baa31e285b9d57aaa14ea1ac2e6aaf
Josivaldo Soares <77576339+JosivaldoS@users.noreply.github.com>
2025-02-16 13:07:57 -0300

    Estilizando a aba sobre mim
    Adicionei estilização da área Sobre mim
    Adicionei uma imagem para acompanhar o texto
    Coloquei o texto lado a lado da imagem

--- src/components/Layout/ThirdSection/ThirdSection.jsx
+++ src/components/Layout/ThirdSection/ThirdSection.jsx
@@ -1,18 +1,22 @@
+import computador from '../../../assets/Computador.jpg'
+
 
 export default function ThirdSection(){
     // Sobre mim
     return(
         <main class='bg-CorPreta text-white w-screen h-[800px] overflow-hidden'>
             <h2 class='text-center text-6xl mt-9'>Sobre mim</h2>
-            <div>
-                <div>
-                    <p>Sou Desenvolvedor Front End, especializado em React, e atualmente curso Análise e Desenvolvimento de Sistemas. Embora ainda não tenha experiência profissional na área, estou constantemente aprimorando minhas habilidades por meio de projetos práticos e estudos contínuos. Sou proativo, dedicado e estou ansioso para aplicar meu conhecimento em um ambiente profissional, contribuindo com minha paixão por tecnologia e aprendizado constante. 🚀
+            <div class='flex justify-center pt-20'>
+                <div class='w-[850px] bg-[#0F110E] '>
+                    <p class='text-2xl p-6'>
+                        Sou Desenvolvedor Front End, especializado em React, e atualmente curso Análise e Desenvolvimento de Sistemas. Embora ainda não tenha experiência profissional na área, estou constantemente aprimorando minhas habilidades por meio de projetos práticos e estudos contínuos. Sou proativo, dedicado e estou ansioso para aplicar meu conhecimento em um ambiente profissional, contribuindo com minha paixão por tecnologia e aprendizado constante. 🚀
                     </p>
                 </div>
                 <div>
                     <img 
-                        src="https://placehold.co/400" 
-                        alt="" 
+                        src={computador} 
+                        alt=""
+                        width={350} 
                     />
                 </div>
             </div>

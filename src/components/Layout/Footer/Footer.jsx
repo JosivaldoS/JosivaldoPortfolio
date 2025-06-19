c9e7232d364b906ec497c06f256b9092668a83ab
Josivaldo Soares <77576339+JosivaldoS@users.noreply.github.com>
2025-06-19 10:18:17 -0300

    Estilizei o Footer
    Footer agora está estilizando faltando apenas atualizar os links utilizando o React Router

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,20 +1,37 @@
 import iconeInstagram from "../../../assets/icons8-instagram.svg"
+import iconeLinkedin from "../../../assets/iconLinkedin.svg"
+import iconeGithub from "../../../assets/iconGithub.svg"
 
 function Footer(){
     return(
         <footer class="bg-gray-800 text-white w-full text-center p-5">
             <nav>
-                <h3>Minhas redes Sociais:</h3>
+                <h3 className="text-xl">Minhas redes Sociais:</h3>
                 <ul>
-                    <li>
-                        Instagram 
+                    <li className="flex justify-center">
                         <img 
                             src={iconeInstagram} 
                             alt="Icone do instagram"
+                            className="w-6"
                         />
+                        <p className="p-2 text-lg">Instagram</p>
                     </li>
-                    <li>Linkedin</li>
-                    <li>Github</li>
+                    <li className="flex justify-center">
+                        <img 
+                            src={iconeLinkedin} 
+                            alt="Icone do instagram"
+                            className="w-6"
+                        />
+                        <p className="p-2 text-lg">Linkedin</p>
+                    </li>
+                    <li className="flex justify-center">
+                        <img 
+                            src={iconeGithub} 
+                            alt="Icone do instagram"
+                            className="w-6"
+                        />
+                        <p className="p-2 text-lg">Github</p>
+                    </li>
                 </ul>
             </nav>
         </footer>

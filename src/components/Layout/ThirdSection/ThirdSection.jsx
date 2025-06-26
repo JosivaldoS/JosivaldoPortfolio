4161b7b8e1b3acbb6abbd7eb7f47a06de785ff6c
Josivaldo Soares <77576339+JosivaldoS@users.noreply.github.com>
2025-06-26 13:27:42 -0300

    Estilizando a aba Sobre Mim
    Deixei mais responsiva para dispositivos mobiles, assim como texto e layout do site

--- src/components/Layout/ThirdSection/ThirdSection.jsx
+++ src/components/Layout/ThirdSection/ThirdSection.jsx
@@ -4,20 +4,20 @@ import computador from '../../../assets/ComputadorSm.jpg'
 export default function ThirdSection(){
     // Sobre mim
     return(
-        <main class='bg-CorPreta text-white w-screen h-auto overflow-hidden pb-10'>
-            <h2 class='text-center text-6xl mt-9'>Sobre mim</h2>
-            <div class='flex justify-center pt-20  flex-col items-center md:flex-row'>             
-                <div className='p-3 pb-0 flex items-center'>
+        <main class='bg-CorPreta text-white w-screen h-auto overflow-hidden pb-10 pt-10'>
+            <h2 class='text-center text-6xl'>Sobre mim</h2>
+            <div class='flex justify-center pt-10  flex-col items-center md:flex-row'>             
+                <div className='p-5 flex flex-col items-center'>
                     <img 
                         src={computador} 
                         alt="Imagem de um laptop com a tampa um pouco levantada"
                         className='w-96' 
                     />
-                </div>
-                <div class='bg-[#0F110E] p-6'>
-                    <p class='text-2xl'>
+                    <div class='bg-[#0F110E] p-6'>
+                    <p class='text-xl text-center'>
                         Sou Desenvolvedor Front End, especializado em React, e atualmente curso Análise e Desenvolvimento de Sistemas. Embora ainda não tenha experiência profissional na área, estou constantemente aprimorando minhas habilidades por meio de projetos práticos e estudos contínuos. Sou proativo, dedicado e estou ansioso para aplicar meu conhecimento em um ambiente profissional, contribuindo com minha paixão por tecnologia e aprendizado constante. 🚀
                     </p>
+                </div>
                 </div>
             </div>
         </main>

b732c5b5fe71d05560dc1726f32fbd255bd6fa49
Josivaldo Soares <77576339+JosivaldoS@users.noreply.github.com>
2025-06-27 00:59:09 -0300

    Alterando os links
    Alterei e atualizei os links utilizados no footer

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -5,34 +5,46 @@ import iconeGithub from "../../../assets/iconGithub.svg"
 function Footer(){
     return(
         <footer class="bg-gray-800 text-white w-full text-center p-5">
+            <h3 className="text-xl">Minhas redes Sasociais:</h3>
             <nav>
-                <h3 className="text-xl">Minhas redes Sociais:</h3>
-                <ul>
-                    <li className="flex justify-center">
-                        <img 
-                            src={iconeInstagram} 
-                            alt="Icone do instagram"
-                            className="w-6"
-                        />
-                        <p className="p-2 text-lg">Instagram</p>
-                    </li>
-                    <li className="flex justify-center">
-                        <img 
-                            src={iconeLinkedin} 
-                            alt="Icone do instagram"
-                            className="w-6"
-                        />
-                        <p className="p-2 text-lg">Linkedin</p>
-                    </li>
-                    <li className="flex justify-center">
-                        <img 
-                            src={iconeGithub} 
-                            alt="Icone do instagram"
-                            className="w-6"
-                        />
-                        <p className="p-2 text-lg">Github</p>
-                    </li>
-                </ul>
+                <a 
+                    className="flex justify-center"
+                    href="https://www.instagram.com/devjunin/?next=%2F"
+                    target="_blank"
+                    rel="noreferrer">
+                    <img 
+                        src={iconeInstagram} 
+                        alt="Icone do instagram"
+                        className="w-6"
+                    />
+                    <p className="p-2 text-lg">Instagram</p>
+                </a>
+
+                <a 
+                    href="https://www.linkedin.com/in/josivaldo-junior/"
+                    className="flex justify-center"
+                    rel="noreferrer"
+                    target="_blank">
+                    <img 
+                        src={iconeLinkedin} 
+                        alt="Icone do Linkedin"
+                        className="w-6"
+                    />
+                    <p className="p-2 text-lg">Linkedin</p>
+                </a>
+
+                <a 
+                    href="github.com/JosivaldoS"
+                    className="flex justify-center"
+                    rel="noreferrer"
+                    target="_blank">
+                    <img 
+                        src={iconeGithub} 
+                        alt="Icone do Github"
+                        className="w-6"
+                    />
+                    <p className="p-2 text-lg">Github</p>
+                </a>
             </nav>
         </footer>
     )
